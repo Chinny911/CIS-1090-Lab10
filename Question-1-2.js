813d512d96244c0e476df69bf4185e00c55f7459
cco04290 <cco04290@castleton.edu>
2022-11-07 10:52:25 -0500

    Lab 10-final

--- Question-1-2.js
+++ Question-1-2.js
@@ -1,4 +1,8 @@
 //❓❓ LAB QUESTION 1, 2:
-let student = {};
+let student = {
+    firstName: "Chinny",
+    lastName: "Okeke",
+    classes: ["Problem Solving with Computers", "Connections Seminar 2", "Effective Speaking", "Intro to Study of Community","Women and Genders Studies" ]
+};
 
 console.log(student);
--- Question-3-4-5-6-7-8.js
+++ Question-3-4-5-6-7-8.js
@@ -21,5 +21,13 @@ let myFish = {
 }
 
 //❓❓ LAB QUESTION 6, 7, 8:
-console.log("Speak, " + myCat.name );
-myCat.speak();
+function introduce (myPet) {
+    console.log("speak," + myPet.name);
+    myPet.speak();
+}
+
+introduce(myCat);
+introduce(myDog);
+introduce(myFish);
+//console.log("Speak, " + myCat.name );
+//myCat.speak();
